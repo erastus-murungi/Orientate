01439ea3b0e471f733e2c454c4ab5697a3ef9708
Erastus Murungi <erastusmurungi@fb.com>
2020-07-15 07:29:52 -0700

    imported login and home activity templates

---
+++ app/src/main/java/com/erastus/orientate/login/StudentLoginActivity.java
@@ -0,0 +1,16 @@
+package com.erastus.orientate.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.erastus.orientate.R;
+
+public class StudentLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+}
